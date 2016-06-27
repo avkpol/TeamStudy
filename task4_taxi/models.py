e697c7cccab071eb4a05eced4aab639c55ce8fed
Andriy <andriykrav@gmail.com>
2016-06-28 00:28:57 +0300

    task4_taxi updated2

--- task4_taxi/models.py
+++ task4_taxi/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.timezone import now, timedelta
+from django.utils.timezone import now
 from django.utils import timezone
 
 MESAGE_TEXT = '''
@@ -10,18 +10,17 @@ MESAGE_TEXT = '''
 class Driver(models.Model):
 
     name = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to='img/', blank=True, null=True )
+    photo = models.ImageField(upload_to='img/', blank=True, null=True)
     email = models.EmailField(null=True, blank=True)
     skype = models.CharField(max_length=50, null=True, blank=True)
     mobile = models.IntegerField(null=True, blank=True)
-    fulfOrder = models.ForeignKey('CltOrder')
+    fulf_order = models.ForeignKey('CltOrder')
 
     def __unicode__(self):
         return unicode(self.last_name)
 
 
 class CltOrder(models.Model):
-
 
     price = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
     distance = models.PositiveIntegerField()
@@ -31,25 +30,29 @@ class CltOrder(models.Model):
     destination = models.CharField(max_length=100)
 
     def all(self):
+
         return self.filter(payed=True)
 
     def after30(self):
-        orderDate = CltOrder.objects.filter().order_by(self.orderDate)
-        now = timezone.now()
-        after30 = orderDate + timezone.timedelta(days=30)
-        if now == after30:
+
+        order_date = CltOrder.objects.filter().order_by(self.orderDate)
+        time_now = timezone.now()
+        after30 = order_date + timezone.timedelta(days=30)
+        if time_now == after30:
             return True
 
     def message(self):
-        if self.after30 == True:
+
+        if self.after30 is True:
             return ('Dear %s,' + MESAGE_TEXT %(self.name))
 
-
     def client_name(self):
+
         self.name = CltOrder.objects.filter(clt=self.client)
         return self.name
 
     def __unicode__(self):
+
         return '%s%s' %(self.client, self.destination)
 
 
@@ -64,14 +67,14 @@ class Client(models.Model):
 
         orders = CltOrder.objects.all()
         for self.order in orders:
-            self.order+=order
+            self.order += order
         return self.order
 
     def every_5(self):
-        if self.order_count==5:
+
+        if self.order_count == 5:
             return True
 
     def __unicode__(self):
+
         return unicode(self.name)
-
-
